bec86b05fe089916e34060450dded986a645ab40
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-07 14:08:35 +0900

    Pin stub versions

--- Lambda/builder/toolchain/PreviewStub/Package.swift
+++ Lambda/builder/toolchain/PreviewStub/Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ModuleBuilder",
     dependencies: [
-        // FIXME: Please replace upstream repo after #271 will be merged
-        .package(name: "Tokamak", url: "https://github.com/kateinoigakukun/Tokamak", .branch("allow-mount-non-body")),
+        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", .branch("main")),
     ],
     targets: [
         .target(name: "PreviewStub", dependencies: [
