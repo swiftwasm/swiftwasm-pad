df5a00e2d0deda49c98fcb49d689bbc87c07d957
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-08 15:49:38 +0900

    Remove self-managed dependency installer

--- Frontend/scripts/install-dependency.sh
+++ Frontend/scripts/install-dependency.sh
@@ -7,12 +7,4 @@ frontend_root="$(cd "$(dirname $0)/../" && pwd)"
 
 third_party_dir=$frontend_root/third-party
 
-if [ ! -e $third_party_dir/$codemirror_version ]; then
-  (cd $third_party_dir && \
-    curl $codemirror_url -o $codemirror_version.zip && \
-    unzip $codemirror_version.zip && \
-    ln -sf $codemirror_version codemirror
-  )
-fi
-
 "$frontend_root/../scripts/install-toolchain.sh"
