b8fb1e2906418729f0a4ba81b88448e71f312ede
Yuta Saito <kateinoigakukun@gmail.com>
2020-09-08 15:51:07 +0900

    Move style file

--- Frontend/web/index.js
+++ Frontend/web/index.js
@@ -1,10 +1,11 @@
+import "codemirror/theme/lucario.css"
+import "codemirror/lib/codemirror.css"
+import "static/style.css"
+
 import { SwiftRuntime } from "javascript-kit-swift";
 import { WASI } from "@wasmer/wasi";
 import { WasmFs } from "@wasmer/wasmfs";
 import * as path from "path-browserify"
-import "codemirror/theme/lucario.css"
-import "codemirror/lib/codemirror.css"
-import "../static/style.css"
 import * as CodeMirror from "codemirror/lib/codemirror"
 import "codemirror/mode/swift/swift"
 
