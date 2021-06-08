31b7a03ed0914ea85c1ad62ebf8e0f31f0b8b8d4
Max Desiatov <max@desiatov.com>
2021-06-08 10:28:53 +0100

    Update dependencies in `Frontend/Package.swift`

--- Frontend/Package.swift
+++ Frontend/Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "JavaScriptKit", url: "https://github.com/swiftwasm/JavaScriptKit", .exact("0.5.0")),
-        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", .branch("main")),
-        .package(name: "ChibiLink", url: "https://github.com/kateinoigakukun/chibi-link", .branch("master")),
+        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", .upToNextMinor(from: "0.3.0")),
+        .package(name: "ChibiLink", url: "https://github.com/kateinoigakukun/chibi-link", .exact("1.0.0")),
     ],
     targets: [
         .target(
