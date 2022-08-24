985540962cda4a37a0e513ec0b9824cc567cad70
Yuta Saito <kateinoigakukun@gmail.com>
2022-08-24 17:35:59 +0900

    WIP

--- CompileAPI/scripts/build-and-package.sh
+++ CompileAPI/scripts/build-and-package.sh
@@ -5,6 +5,7 @@ workspace="$(cd "$(dirname $0)/../" && pwd)"
 scripts="$workspace/scripts"
 preview_dir="$workspace/../PreviewSystem"
 builder_dir="$scripts/builder"
+SWIFT_TAG=swift-wasm-5.6-SNAPSHOT-2022-06-30-a
 
 echo "-------------------------------------------------------------------------"
 echo "building PreviewSystem"
@@ -13,22 +14,33 @@ echo "-------------------------------------------------------------------------"
 echo "done"
 
 echo "-------------------------------------------------------------------------"
-echo "preparing docker build image"
+echo "downloading cross compiler toolchain for amazonlinux2"
 echo "-------------------------------------------------------------------------"
-docker build "$builder_dir" -t tokamak-pad-package-builder
+
+mkdir -p $workspace/toolchain
+if [[ ! -e $workspace/toolchain/$SWIFT_TAG-amazonlinux2_x86_64.tar.gz ]]; then
+  curl -LO --output-dir $workspace/toolchain \
+    https://github.com/swiftwasm/swift/releases/download/$SWIFT_TAG/$SWIFT_TAG-amazonlinux2_x86_64.tar.gz
+fi
+
+if [[ ! -e $workspace/toolchain/$SWIFT_TAG ]]; then
+  tar xfzv $workspace/toolchain/$SWIFT_TAG-amazonlinux2_x86_64.tar.gz -C $workspace/toolchain
+fi
+
 echo "done"
 
 echo "-------------------------------------------------------------------------"
 echo "building \"$executable\" lambda"
 echo "-------------------------------------------------------------------------"
-docker run --rm -v "$workspace":/workspace -w /workspace tokamak-pad-package-builder \
-       bash -cl "swift build --product $executable -c release"
+docker run --rm -v "$workspace":/workspace -w /workspace --platform linux/amd64 \
+  swift:5.6-amazonlinux2 bash -cl "swift build --product $executable -c release"
 echo "done"
 
 echo "-------------------------------------------------------------------------"
 echo "packaging \"$executable\" lambda"
 echo "-------------------------------------------------------------------------"
-docker run --rm -v "$workspace":/workspace/Lambda -v "$workspace/../PreviewSystem":/workspace/PreviewSystem \
-       -w /workspace/Lambda tokamak-pad-package-builder \
+set -x
+docker run --rm -v "$workspace":/workspace/Lambda -v "$workspace/../PreviewSystem":/workspace/PreviewSystem -v "$workspace/toolchain/$SWIFT_TAG":/home/work/toolchain \
+       -w /workspace/Lambda --platform linux/amd64 swift:5.6-amazonlinux2 \
        bash -cl "./scripts/package.sh $executable"
 echo "done"
